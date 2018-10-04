8f64aad7cecf65517bed9abcfb506a1c1262cb9c
Vedang Wartikar <vedangwartikar17@gmail.com>
2018-10-04 23:14:33 +0530

    visualization

---
+++ knn_visualization.py
@@ -0,0 +1,9 @@
+import mglearn
+
+#Prediction based on the nearest data point
+mglearn.plots.plot_knn_classification(n_neighbors = 1)
+
+#Prediction based on the most frequently occured class considering k-neighbors
+mglearn.plots.plot_knn_classification(n_neighbors = 2)
+
+mglearn.plots.plot_knn_classification(n_neighbors = 3)
