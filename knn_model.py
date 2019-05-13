a02b197db08a04a49cd04a81a693a03135a0225c
Vedang Wartikar <vedangwartikar17@gmail.com>
2019-05-14 01:16:14 +0530

    Update knn_model.py

--- knn_model.py
+++ knn_model.py
@@ -1,6 +1,28 @@
 import numpy as np
 import pandas as pd
-import mglearn
+from scipy.spatial import distance
+
+class CustomKNN:
+	def fit(self, train_data, train_target):
+		self.train_data = train_data
+		self.train_target = train_target
+
+	def predict(self, test_data):
+		predictions =[]
+		for row in test_data:
+			label = self.closest(row)
+			predictions.append(label)
+			return predictions
+
+	def closest(self, row):
+		closest_distance = distance.euclidean(row, self.train_data[0])
+		closest_index = 0
+		for i in range(1, len(self.train_data)):
+			dist = distance.euclid(row, self.train_data[i])
+			if dist < closest_distance:
+				closest_distance = dist
+				closest_index = i
+		return self.train_target[closest_index]
 
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
@@ -20,8 +42,8 @@ iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
 grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
 
 #Building K-nearest neighbor model
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors = 1)
+# from sklearn.neighbors import KNeighborsClassifier
+knn = CustomKNN(n_neighbors = 1)
 
 knn.fit(X_train, y_train)
 
